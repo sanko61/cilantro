3b6e9383eae9107938fddc4c6bdca69741fc3ff3
Davis Haba <davis.haba76@gmail.com>
2018-10-19 12:09:09 -0700

    test with 6 nodes...

--- tests/integration/networking/test_large_network.py
+++ tests/integration/networking/test_large_network.py
@@ -59,7 +59,7 @@ class TestLargeNetwork(MPTestCase):
     #     for i, n in enumerate(all_nodes):
     #         n.add_sub_socket(secure=True)
     #         node_vk = all_vks[i]
-    #         for vk in VKBook.get_all():
+    #         for vk in all_vks:
     #             if vk == node_vk: continue
     #             n.connect_sub(vk=vk)
     #
@@ -72,30 +72,29 @@ class TestLargeNetwork(MPTestCase):
     #     self.start(timeout=20)
 
     @vmnet_test(run_webui=True)
-    def test_2_2(self):
-        """
-        Tests creating a network with 2 Masternodes, 2 Witnesses, and 4 Delegates. Ensures everyone can connect to
-        each other.
-        """
+    def test_2_2_2(self):
         def assert_sub(test_obj):
             c_args = test_obj.handle_sub.call_args_list
-            assert len(c_args) == 3, "Expected 3 messages (one from each node). Instead, got:\n{}".format(c_args)
+            assert len(c_args) == 7, "Expected 7 messages (one from each node). Instead, got:\n{}".format(c_args)
 
         BLOCK = False
         time.sleep(1)
 
         mn_0 = MPPubSubAuth(sk=TESTNET_MASTERNODES[0]['sk'], name='[node_1]MN_0', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
-        time.sleep(2)  # Pause after first MN boots (so we are extra sure he will be available for discovery)
+        time.sleep(3)  # Pause after first MN boots (so we are extra sure he will be available for discovery)
         mn_1 = MPPubSubAuth(sk=TESTNET_MASTERNODES[1]['sk'], name='[node_2]MN_1', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
 
         wit_0 = MPPubSubAuth(sk=TESTNET_WITNESSES[0]['sk'], name='[node_3]WITNESS_0', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
         wit_1 = MPPubSubAuth(sk=TESTNET_WITNESSES[1]['sk'], name='[node_4]WITNESS_1', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
 
+        del_0 = MPPubSubAuth(sk=TESTNET_DELEGATES[0]['sk'], name='[node_5]DELEGATE_0', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
+        del_1 = MPPubSubAuth(sk=TESTNET_DELEGATES[1]['sk'], name='[node_6]DELEGATE_1', config_fn=config_sub, assert_fn=assert_sub, block_until_rdy=BLOCK)
+
         time.sleep(10*CI_FACTOR)  # Nap while nodes hookup
 
-        all_nodes = (mn_0, mn_1, wit_0, wit_1)
+        all_nodes = (mn_0, mn_1, wit_0, wit_1, del_0, del_1)
         all_vks = (TESTNET_MASTERNODES[0]['vk'], TESTNET_MASTERNODES[1]['vk'], TESTNET_WITNESSES[0]['vk'],
-                   TESTNET_WITNESSES[1]['vk'])
+                   TESTNET_WITNESSES[1]['vk'], TESTNET_DELEGATES[0]['vk'], TESTNET_DELEGATES[1]['vk'],)
 
         # Each node PUBS on its own IP
         for n in all_nodes:
@@ -115,7 +114,7 @@ class TestLargeNetwork(MPTestCase):
         for n in all_nodes:
             n.send_pub("hi from {} with ip {}".format(n.name, n.ip).encode())
 
-        self.start(timeout=15)
+        self.start(timeout=10)
 
 
 if __name__ == '__main__':
