24d70a46ad1e92bbbad80d36eecc337267696427
Colin McGrath <cpmcgrat@gmail.com>
2018-12-21 12:59:51 -0800

    Updated logger to take in process name for stream to ensure no collisions on SSL sockets

--- cilantro/logger/base.py
+++ cilantro/logger/base.py
@@ -8,6 +8,7 @@ from os.path import dirname
 from logging.handlers import TimedRotatingFileHandler
 import cilantro
 from vmnet.cloud.aws import AWSCloudWatchHandler
+import multiprocessing
 
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 logging.getLogger('boto3').setLevel(logging.WARNING)
@@ -152,8 +153,10 @@ def get_logger(name=''):
         ColoredStreamHandler()
     ]
 
+    pname = multiprocessing.current_process().name
+
     if os.getenv('VMNET_CLOUD'):
-        filehandlers.append(AWSCloudWatchHandler())
+        filehandlers.append(AWSCloudWatchHandler(pname))
 
     logging.basicConfig(
         format=format,
