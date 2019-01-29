71bf8707c86d122eabce11fd715d495e777c70a9
Davis Haba <davis.haba76@gmail.com>
2019-01-28 16:49:28 -0800

    comments in block agg / block man

--- cilantro/nodes/delegate/block_manager.py
+++ cilantro/nodes/delegate/block_manager.py
@@ -276,9 +276,9 @@ class BlockManager(Worker):
 
     def recv_block_notif(self, block: BlockMetaData):
         self.db_state.is_catchup_done = False
+        # TODO call run_catchup() if catchup_manager is not already catching up
         if self.db_state.catchup_mgr.recv_new_blk_notif(block):
             self.set_catchup_done()
-   
 
     def handle_router_msg(self, frames):
         envelope = Envelope.from_bytes(frames[-1])
